0d51dfe2037d133820255b190e6b47fe66abc1a0
Ayan Dave <daveayan@gmail.com>
2013-03-26 23:08:05 -0400

    removed joda temporarity

--- src/main/scala/com/daveayan/croutons/Providers.scala
+++ src/main/scala/com/daveayan/croutons/Providers.scala
@@ -1,24 +1,19 @@
 package com.daveayan.croutons
-import org.joda.time.DateTime
 import java.util.Date
 
 case class DateProvider (number_of: Int){
 	def weeks() : Date = {
-	  def dt: DateTime = DateTime.now()
-	  return dt.toDate()
+	  return new Date()
 	}
 	def weeks_except_weekends() : Date = {
-	  def dt: DateTime = DateTime.now()
-	  return dt.toDate()
+	  return new Date()
 	}
 	
 	def months() : Date = {
-	  def dt: DateTime = DateTime.now()
-	  return dt.toDate()
+	  return new Date()
 	}
 	def months_except_weekends() : Date = {
-	  def dt: DateTime = DateTime.now()
-	  return dt.toDate()
+	  return new Date()
 	}
 }
 
