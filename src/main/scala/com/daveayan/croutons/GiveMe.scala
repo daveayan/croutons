7b5eaca3025b2d2080e6e89eefe0f50c7453103c
Ayan Dave <daveayan@gmail.com>
2013-04-03 19:58:01 -0400

    Javadoc

--- src/main/scala/com/daveayan/croutons/GiveMe.scala
+++ src/main/scala/com/daveayan/croutons/GiveMe.scala
@@ -1,7 +1,22 @@
 package com.daveayan.croutons
-import com.daveayan.croutons.DateProvider
-import com.daveayan.croutons.StringProvider
 
+/**
+ * Usage: 
+ * GiveMe.a_date_in_next(4).weeks()
+ * GiveMe.a_date_in_next(4).weeks_except_weekend()
+ * 
+ * GiveMe.a_date_in_next(4).months()
+ * GiveMe.a_date_in_next(4).months_except_weekend()
+ * 
+ * GiveMe.a_date_in_next(4).years()
+ * GiveMe.a_date_in_next(4).years_except_weekend()
+ * 
+ * GiveMe.text_with_(1).lines()
+ * GiveMe.text_with_(2).lines()
+ * 
+ * GiveMe.text_with_(1).paragraphs()
+ * GiveMe.text_with_(2).paragraphs()
+ */
 object GiveMe {
   def text_with_(number_of: Int) : StringProvider = {
     return new StringProvider(number_of)
