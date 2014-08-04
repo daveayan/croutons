54959122552a2cbfaf85f8bfb0490426988ab042
Ayan Dave <daveayan@gmail.com>
2014-08-03 22:56:29 -0400

    onlyforjavadoc

---
+++ src/main/java/croutons/OnlyForJavadoc.java
@@ -0,0 +1,10 @@
+package croutons;
+
+/**
+ * This file is added only so that javadoc is generated until I figure out some other way.
+ * @author daveayan
+ *
+ */
+public class OnlyForJavadoc {
+
+}
